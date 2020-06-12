170eb8bb3572cb72e06c1c2df1a03d13bdd174d9
Daniel Woldegiorgis <dww2@williams.edu>
2020-06-12 00:53:07 -0400

    Updates

---
+++ precache-manifest.5c2cd669081af38afd56d63c0683a111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f5135306a2508644bb7cd9ff4b7cd7",
+    "url": "/game-of-life-react/index.html"
+  },
+  {
+    "revision": "6055adda71b230f6c9c2",
+    "url": "/game-of-life-react/static/css/main.14bf5bfb.chunk.css"
+  },
+  {
+    "revision": "5458103f3cec8a062245",
+    "url": "/game-of-life-react/static/js/2.a02003af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life-react/static/js/2.a02003af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6055adda71b230f6c9c2",
+    "url": "/game-of-life-react/static/js/main.7a7e17bc.chunk.js"
+  },
+  {
+    "revision": "5fb106843b414920911c",
+    "url": "/game-of-life-react/static/js/runtime-main.4358f0c3.js"
+  }
+]);
