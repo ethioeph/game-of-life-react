e23d6080080ab05814148fc8afb4450c4b3525b1
Daniel Woldegiorgis <dww2@williams.edu>
2020-06-11 23:33:38 -0400

    finished version 1

--- src/index.js
+++ src/index.js
@@ -2,30 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-//This is a tic-tac-toe game that uses a 5 x 5 board for playing.
-/*
-//this is a class component, but since we now no longer have any state in this component we can re-write this as a functional component as shown below. 
-class Square extends React.Component {
-  render() {
-    return (
-      //here note that we are passing in a function as the onClick property(prop).
-      <button className= "square" 
-              onClick  = {() => this.props.onClick()}
-              > 
-        {this.props.value} 
-      </button>                  
-    );
-  }
-}
-*/
-
-//functional components takes props as input and returns 'what should be rendered'.
+//functional component for a single cell. Also determines color of cell (0: white, 1: pink).
 function Square(props){
   let cellColor;
-
-
   cellColor = props.value === 0 ? "square" : "square-red";
-
   return (    
     <button className={cellColor} onClick={props.onClick}>
         {props.value}
@@ -33,85 +13,67 @@ function Square(props){
   )
 }
 
+//class component for the entire board.
 class Board extends React.Component {
 
+  //renders a cell and determines what happens when it is clicked.
   renderSquare(i) {
     return <Square value={this.props.squares[i]}
-                   onClick={()=> this.props.onClick(i)}
-                   />;//passing in a property(prop) called value to the square component.
+                   onClick={()=> this.props.onClick(i)}/>;
   }
 
-  render() {
+  //renders the entire board, by rendering each cell in a grid layout.
+  render() {    
+    var list = [];
+    for (var i = 0; i < 225; i++) {
+      list.push(i);
+    } 
+
     return (
-      <div>
-        <div className="board-row">          
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(5)}
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-          {this.renderSquare(9)}
-
-        </div>
-        <div className="board-row">
-          {this.renderSquare(10)}
-          {this.renderSquare(11)}
-          {this.renderSquare(12)}
-          {this.renderSquare(13)}
-          {this.renderSquare(14)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(15)}
-          {this.renderSquare(16)}
-          {this.renderSquare(17)}
-          {this.renderSquare(18)}
-          {this.renderSquare(19)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(20)}
-          {this.renderSquare(21)}
-          {this.renderSquare(22)}
-          {this.renderSquare(23)}
-          {this.renderSquare(24)}
-        </div>
-
+      <div style={
+        {display: 'grid',
+        gridTemplateColumns: `repeat(${15},34px)`}}>
+          {list.map(num => <div key={`${num}`}> {this.renderSquare(num)} </div>)}
       </div>
-    );
+    )
   }
 }
 
 class Game extends React.Component {
-  //this is used to manage state of the game (e.g. moving between histories)
+  //the state of the game is housed in this component. 
   constructor(props) {
     super(props);
+    
     this.state = {
-        squares: Array(100).fill(0),
-        width: 10,
+        squares: Array(225).fill(0),
+        width: 15,
         interval: null,
     }
   }
 
+  //used to remove any live cells from the board.
+  clearGrid(){
+    const emptySquares = Array(225).fill(0);
+    this.setState({squares: emptySquares});
+  }
 
+  //used to bring random cells to life
+  populateRandom(){
+    let newSquares = Array.from({length: 225}, () => Math.round(Math.random()));
+    console.log(newSquares);
+    this.setState({squares: newSquares});
+  }
+
+  //determines which cell is clicked and toggles it's value (0 <-> 1).
   handleClick(i) {
-    console.log("handling click" + i);
     const squares = this.state.squares;
     let squaresCopy = [...squares];
     squaresCopy[i] = squares[i] ? 0 : 1;
     this.setState({squares: squaresCopy});
   }
 
-  stopSimulation(){
-    clearInterval(this.state.interval);
-  }
-
+  //used to initiate the game-of-life simulation
   startSimulation(){
-    console.log("started simulation");
     if(this.state.interval){
       clearInterval(this.state.interval);
     }
@@ -119,34 +81,37 @@ class Game extends React.Component {
     this.setState({interval: interval});
   }
 
+  //used to stop the game-of-life simulation
+  stopSimulation(){
+    clearInterval(this.state.interval);
+  }
+
+  //enforces the rules of conway's game of life on the current board state.
   simulate(){
-    console.log(this.state);
     const squares = this.state.squares;
     let squaresCopy = [...squares];
     const width = this.state.width; 
 
-    //run one iteration of the game of life (use setState to update the array.)
+    //run one iteration of the game of life 
     let liveNeighborsCount = 0;
-    for(let row = 1; row < width - 1; row++){
-      for(let col = 1; col < width - 1; col++){
+    for(let row = 0; row < width ; row++){
+      for(let col = 0; col < width ; col++){
         //looking at each of the eight neighbors and keeping track of the liveNeighbors
-        liveNeighborsCount = squaresCopy[width * row - 1 + col - 1] +
-        squaresCopy[width * row - 1 + col] +
-        squaresCopy[width * row - 1 + col + 1] +
+        let topLeft = row>0 && col > 0 ? squares[width * (row - 1) + (col - 1)] : 0;
+        let left = row>0 ? squares[width * (row - 1) + col] : 0;
+        let bottomLeft = row>0 && col < width -1 ? squares[width * (row - 1) + (col + 1)]: 0;
 
-        squaresCopy[width * row + col - 1] +
-        squaresCopy[width * row + col + 1 ] +
+        let top = col > 0? squares[width * row + (col - 1)] : 0;
+        let bottom = col < width - 1? squares[width * row + (col + 1)] :0;
 
-        squaresCopy[width * row + 1 + col - 1] +
-        squaresCopy[width * row + 1 + col] +
-        squaresCopy[width * row + 1 + col + 1];
+        let topRight = row < width - 1 && col > 0 ? squares[width * (row + 1) + (col - 1)] : 0;
+        let right = row < width - 1? squares[width * (row + 1) + col] : 0 ;
+        let bottomRight = row < width - 1 && col < width - 1? squares[width * (row + 1) + (col + 1)] : 0;
+        liveNeighborsCount = topLeft + left + bottomLeft + top + bottom + topRight + right + bottomRight;
 
-        if(squaresCopy[width * row + col] === 1){//live cell
-          if(liveNeighborsCount < 2){
-            squaresCopy[width * row + col] = 0;//dies by underpopulation
-          }
-          else if(liveNeighborsCount > 3){
-            squaresCopy[width * row + col] = 0;//dies by overpopulation
+        if(squares[width * row + col] === 1){//live cell
+          if(liveNeighborsCount < 2 || liveNeighborsCount > 3){
+            squaresCopy[width * row + col] = 0;//dies by underpopulation or overpopulation
           }
         }
         else{
@@ -157,20 +122,35 @@ class Game extends React.Component {
         liveNeighborsCount = 0;
       }
     }
-
-    console.log("Calling Set State!");
+    //update the grid with new grid configuration.
     this.setState({squares: squaresCopy});
-    // ,()=> 
-    // setTimeout(this.simulate, 1000, this.state))
   }
 
+  //used to render the entire game 
   render() {
-    //caching in memory the values in global game state
-
     return (
-      <>
-      <button onClick={() => this.startSimulation()}>Start Simulation</button>
-      <button onClick={() =>  this.stopSimulation()}>Stop Simulation</button>
+      <div className="App">
+      <h1 className="title">Conway's Game Of Life</h1>
+      <button className="button-style-green" onClick={() => this.startSimulation()}>Simulate</button>
+      <button className="button-style-red" onClick={() =>  this.stopSimulation()}>Pause</button>
+      <button className="button-style-purple" onClick={() =>  this.populateRandom()}>Random</button>
+      <button className="button-style-clear" onClick={() =>  this.clearGrid()}>Clear</button>
+
+      <div style={{color: 'white'}}>
+        To start/resume the simulation, click on the "simulate" Button.
+      </div>
+
+      <div style={{color: 'white'}}>
+      To stop/pause the simulation, click on the "Pause" button.
+      </div>
+
+      <div style={{color: 'white'}}>
+      To generate a random grid configuration, click on the "random" button.
+      </div>
+
+      <div style={{color: 'white'}}>
+      To clear the board configuration, click on the "clear" button.
+      </div>
 
       <div className="game">
         <div className="game-board">
@@ -179,12 +159,8 @@ class Game extends React.Component {
             onClick={(i) => this.handleClick(i)}
           />
         </div>
-        <div className="game-info">
-          {/* <div>{status}</div> */}
-          {/* <ol className="numbers">{moves}</ol> */}
-        </div>
       </div>
-      </>
+      </div>
     );
   }
 }
